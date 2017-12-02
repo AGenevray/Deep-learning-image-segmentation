21de955b3d46040567091d8e2e26cf3ab4c74756
Kylian VINCENT <kylian.vincent@ensimag.grenoble-inp.fr>
2017-12-02 15:24:44 +0100

    Added unlabelled support and layer correction (suppression of fullt connected)

--- data_utils.py
+++ data_utils.py
@@ -41,8 +41,8 @@ def print_image(image_pl, width, height, num_classes, labelled_im):
     # Plot the obtained image
     plt.subplot(121)
     image_out = np.argmax(image_pl, axis=-1)[0]
-    image = np.full([height, width, 3], 0).astype(np.uint8)
-    for c in range(num_classes+1):
+    image = np.full([height, width, 3], label_colours[num_classes-1]).astype(np.uint8)
+    for c in range(num_classes):
         image[image_out == c] = label_colours[c]
     plt.imshow(image)
     plt.title('Output labels')
@@ -50,8 +50,8 @@ def print_image(image_pl, width, height, num_classes, labelled_im):
     # Plot the labelled image
     plt.subplot(122)
     labelled = np.argmax(labelled_im, axis=-1)[0]
-    true_labels = np.full([height, width, 3], 0).astype(np.uint8)
-    for c in range(num_classes+1):
+    true_labels = np.full([height, width, 3], label_colours[num_classes-1]).astype(np.uint8)
+    for c in range(num_classes):
         true_labels[labelled == c] = label_colours[c]
     plt.imshow(true_labels)
     plt.title('Original image labels')
@@ -59,7 +59,7 @@ def print_image(image_pl, width, height, num_classes, labelled_im):
     
     # Plot the labels legend
     plt.figure(figsize=(12,1))
-    for c in range(num_classes+1):
+    for c in range(num_classes):
         plt.subplot(1, num_classes+1, c+1)
         plt.axis('off')
         plt.imshow(np.full([10, 20, 3], label_colours[c]).astype(np.uint8))
